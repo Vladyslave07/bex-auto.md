2bc561b64c334ae630fd79cae940a16fc4f6510a
Igor_Bashuk <vmefistoz@gmail.com>
2022-09-02 15:03:07 +0200

    19657 - fix

--- resources/views/partials/footer/menu-footer-part.blade.php
+++ resources/views/partials/footer/menu-footer-part.blade.php
@@ -1,4 +1,4 @@
-@if(($item->children) > 0)
+@if(count($item->children) > 0)
     <div class="menu-footer">
         @foreach($item->children as $child)
             <a href="{{ $child['url'] }}">{{ $child['name'] }}</a>
