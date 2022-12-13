9bc4e977d4c027a3ef685d139b622ba9eee4c21f
Аlex Kudriashov <alexkudriashov1985@gmail.com>
2022-12-13 10:34:21 +0200

    21158 Доработки

--- resources/views/partials/index/popular-brand-auto.blade.php
+++ resources/views/partials/index/popular-brand-auto.blade.php
@@ -1,6 +1,6 @@
 @if (count($brands) > 0)
 <div class="popular-brands">
-    <div class="container hidden-xs">
+    <div class="container">
         <div class="main-title text-center">{{ config('settings.popular_brand_auto_title') }}</div>
     </div>
     <div class="list">
