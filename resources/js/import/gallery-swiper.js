1eb908efec4ada2d4c7c2242f6aaf9d3bc6aa5ce
Аlex Kudriashov <alexkudriashov1985@gmail.com>
2023-01-03 11:51:00 +0200

    20831 Переной сайта на прод

--- resources/js/import/gallery-swiper.js
+++ resources/js/import/gallery-swiper.js
@@ -18,12 +18,18 @@ export default function() {
                     video.src = video.dataset.src;
                     video.removeAttribute('data-src');
                 }
+
+                document.querySelector('.gallery-thumbs .item.active').classList.remove('active');
+                document.querySelectorAll('.gallery-thumbs .item')[e.activeIndex].classList.add('active');
             },
         }
     });
 
     document.addEventListener('livewire:load', function () {
         document.querySelectorAll('.gallery-thumbs .item').forEach((btn, i) => btn.addEventListener('click', () => {
+            if(btn.querySelector('.more') && !btn.classList.contains('open')) {
+                return false;
+            }
             btn.closest('.gallery-thumbs').querySelector('.active').classList.remove('active');
             btn.classList.add('active');
             galleryDwiper.slideTo(i);
