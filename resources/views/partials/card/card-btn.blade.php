d3807b0ef150ef07a9adfbbdb2f32aac38a45b86
Igor_Bashuk <igor@redchameleon.com.ua>
2023-02-10 09:35:50 +0100

    21817 - remove btn

--- resources/views/partials/card/card-btn.blade.php
+++ resources/views/partials/card/card-btn.blade.php
@@ -1,4 +1,6 @@
 <button onclick="openModal('#applicationForCar')"
         class="btn">{{ Lang::get('car.detail.buy') }}</button>
-<button onclick="openModal('#applicationForCredit')"
-        class="btn btn-blue">{{ Lang::get('car.detail.buy_in_credit') }}</button>
+@if(\App\Models\Domain::currentDomain()?->slug !== 'kz')
+    <button onclick="openModal('#applicationForCredit')"
+            class="btn btn-blue">{{ Lang::get('car.detail.buy_in_credit') }}</button>
+@endif
