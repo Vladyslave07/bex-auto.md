9b4aca270c884469b96c384e2de19002e2a14fac
Igor_Bashuk <igor@redchameleon.com.ua>
2023-03-09 12:38:18 +0100

    22277 - hide car. form filter

--- resources/views/filters/status.blade.php
+++ resources/views/filters/status.blade.php
@@ -1,11 +1,14 @@
 @if(count($filter['values']) > 0)
     @foreach($filter['values'] as $value)
-        <div class="form-check">
-            <label>
-                <input class="form-radio" wire:click="setFilter('{{ $filter['slug'] }}', '{{ $value['value'] }}')" type="radio" name="status" >
-                {{ \Illuminate\Support\Facades\Lang::get('car.' . $value['value']) }}
-            </label>
-        </div>
+        @if($value['value'])
+            <div class="form-check">
+                <label>
+                    <input class="form-radio" wire:click="setFilter('{{ $filter['slug'] }}', '{{ $value['value'] }}')"
+                           type="radio" name="status">
+                    {{ \Illuminate\Support\Facades\Lang::get('car.' . $value['value']) }}
+                </label>
+            </div>
+        @endif
     @endforeach
 @endif
 <hr class="hr">
