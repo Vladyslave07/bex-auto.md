53b29fd6336b5068cbbfc4c6e5120776c49e63b2
Igor_Bashuk <igor@redchameleon.com.ua>
2023-03-28 17:17:55 +0200

    22406 - change size preview picture

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -321,33 +321,33 @@ class Car extends Model
     }
 
 
-    public function setPreviewImageAttribute($value)
-    {
-        $attribute_name = "preview_image";
-        $disk = "public";
-        $destination_path = Str::replace('_', '', $this->table);
-
-        if ($value == null) {
-            \Storage::disk($disk)->delete($this->{$attribute_name} ?? '');
-            $this->attributes[$attribute_name] = null;
-        } else {
-            if (Str::startsWith($value, 'data:image')) {
-                $ext = 'jpg';
-                if (Str::startsWith($value, 'data:image/png;base64')) $ext = 'png';
-                if (Str::startsWith($value, 'data:image/jpeg;base64')) $ext = 'jpeg';
-                if (Str::startsWith($value, 'data:image/webp;base64')) $ext = 'webp';
-
-                $image = \Image::make($value)->fit(289, 220, function ($constraint) {
-                    $constraint->upsize();
-                })->encode($ext, 90);
-
-                $filename = md5($value . time()) . '.' . $ext;
-                \Storage::disk($disk)->put($destination_path . '/' . $filename, $image->stream());
-                \Storage::disk($disk)->delete($this->{$attribute_name} ?? '');
-                $this->attributes[$attribute_name] = $destination_path . '/' . $filename;
-            } else $this->attributes[$attribute_name] = Str::replace(env('APP_URL') . '/storage', '', $value);
-        }
-    }
+//    public function setPreviewImageAttribute($value)
+//    {
+//        $attribute_name = "preview_image";
+//        $disk = "public";
+//        $destination_path = Str::replace('_', '', $this->table);
+//
+//        if ($value == null) {
+//            \Storage::disk($disk)->delete($this->{$attribute_name} ?? '');
+//            $this->attributes[$attribute_name] = null;
+//        } else {
+//            if (Str::startsWith($value, 'data:image')) {
+//                $ext = 'jpg';
+//                if (Str::startsWith($value, 'data:image/png;base64')) $ext = 'png';
+//                if (Str::startsWith($value, 'data:image/jpeg;base64')) $ext = 'jpeg';
+//                if (Str::startsWith($value, 'data:image/webp;base64')) $ext = 'webp';
+//
+//                $image = \Image::make($value)->fit(289, 220, function ($constraint) {
+//                    $constraint->upsize();
+//                })->encode($ext, 90);
+//
+//                $filename = md5($value . time()) . '.' . $ext;
+//                \Storage::disk($disk)->put($destination_path . '/' . $filename, $image->stream());
+//                \Storage::disk($disk)->delete($this->{$attribute_name} ?? '');
+//                $this->attributes[$attribute_name] = $destination_path . '/' . $filename;
+//            } else $this->attributes[$attribute_name] = Str::replace(env('APP_URL') . '/storage', '', $value);
+//        }
+//    }
 
     /*
     |--------------------------------------------------------------------------
