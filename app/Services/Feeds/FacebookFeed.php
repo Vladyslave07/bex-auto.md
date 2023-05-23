d013fce5219420f1f61b068a76592f9e7bdf8889
Igor_Bashuk <igor@redchameleon.com.ua>
2023-05-23 15:25:36 +0200

    23187 - facebook feed

--- app/Services/Feeds/FacebookFeed.php
+++ app/Services/Feeds/FacebookFeed.php
@@ -51,31 +51,39 @@ class FacebookFeed extends Feed
             File::append($this->getFileName(), '<vehicle_id>' . $car->id . '</vehicle_id>');
             File::append($this->getFileName(), "<description>" .  htmlspecialchars(strip_tags($car->description)) . "</description>");
             File::append($this->getFileName(), '<url>' . LaravelLocalization::getLocalizedUrl($this->getLocale(), route('car_detail', [$car->slug])) . '</url>');
-            File::append($this->getFileName(), "<title>" . htmlspecialchars($car->title) . "</title>");
-            File::append($this->getFileName(), "<price>" . $car->price . " USD</price>");
+            File::append($this->getFileName(), "<title>" . htmlspecialchars($car->titleWithYear) . "</title>");
+            File::append($this->getFileName(), "<price>" . $car->priceFormat . " USD</price>");
+            File::append($this->getFileName(), "<vin>" . $car->vin . "</vin>");
+            File::append($this->getFileName(), "<state_of_vehicle>" . (__('car.' . $car->status) ) . "</state_of_vehicle>");
+
+            // Год
+            $year =  $car->properties->where('slug', 'year')->first()?->pivot->value;
+            File::append($this->getFileName(), "<year>" . $year . "</year>");
+
+            // Марка
+            $brand = $car->properties->where('slug', 'brand')->first()?->pivot->value;
+            File::append($this->getFileName(), "<make>" . htmlspecialchars($brand) . "</make>");
+
+            // Модель
+            $model = $car->properties->where('slug', 'model')->first()?->pivot->value;
+            File::append($this->getFileName(), "<model>" . htmlspecialchars($model) . "</model>");
 
             // Тип кузова
             $carBodyType = $bodyTypes->where('value', $car->properties->where('slug', 'carcase-type')->first()?->pivot->slug)->first();
-            if ($carBodyType) {
-                File::append($this->getFileName(), '<body_style>' . $carBodyType['name'] . '</body_style>');
-            }
+            File::append($this->getFileName(), '<body_style>' . ($carBodyType ? $carBodyType['name'] : '') . '</body_style>');
 
             // Пробег
             $mileage = $car->properties->where('slug', 'mileage')->first()?->pivot->value;
-            if ($mileage) {
-                File::append($this->getFileName(), '<mileage>');
-                File::append($this->getFileName(), '<unit>КМ</unit>');
-                File::append($this->getFileName(), "<value>" . $mileage . "</value>");
-                File::append($this->getFileName(), '</mileage>');
-            }
+            File::append($this->getFileName(), '<mileage>');
+            File::append($this->getFileName(), '<unit>КМ</unit>');
+            File::append($this->getFileName(), "<value>" . ($mileage ?? 0) . "</value>");
+            File::append($this->getFileName(), '</mileage>');
 
             // Фото
             $previewPicture = Storage::url($car->previewPicture);
-            if (strlen($previewPicture) > 0) {
-                File::append($this->getFileName(), '<image>');
-                File::append($this->getFileName(), "<url>" . $previewPicture . "</url>");
-                File::append($this->getFileName(), '</image>');
-            }
+            File::append($this->getFileName(), '<image>');
+            File::append($this->getFileName(), "<url>" . $previewPicture . "</url>");
+            File::append($this->getFileName(), '</image>');
 
             // Close item
             File::append($this->getFileName(), '</listing>');
@@ -92,6 +100,7 @@ class FacebookFeed extends Feed
         return Car::query()
             ->where('domain_id', app('domain')->getDomain()->id)
             ->active()
+            ->take(1)
             ->get();
     }
 }
