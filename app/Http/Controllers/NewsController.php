64e4ecdc7a3beb169b7c8c05705e72a3c8405787
Igor_Bashuk <igor@redchameleon.com.ua>
2023-07-20 23:24:37 +0200

    fix

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -34,7 +34,7 @@ class NewsController extends Controller
 
     public function detail(News $article)
     {
-        if (!$article->forCurrentDomain() || !$article->active()) {
+        if (!$article->forCurrentDomain() || $article->active != 1) {
             abort(404);
         }
 
