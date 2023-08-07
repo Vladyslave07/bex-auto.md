2bdd16d03a2e9b56ebffe45a2b2367f1144ca1b0
Igor_Bashuk <igor@redchameleon.com.ua>
2023-08-07 21:20:59 +0200

    23712 - add middleware for devide databases for each domain

---
+++ app/Http/Middleware/DatabaseMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class DatabaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $currentDomain = $request->getHost();
+
+        // Определить соединение с базой данных на основе домена
+        $connection = $this->resolveConnection($currentDomain);
+
+        // Установить выбранное соединение в конфигурации базы данных Laravel
+        Config::set('database.default', $connection);
+
+        return $next($request);
+    }
+
+    private function resolveConnection($domain): string
+    {
+        // Логика для определения соединения на основе домена
+        switch ($domain) {
+            case 'bex-auto.local':
+                return 'mysql';
+            case 'kz.bex-auto.local':
+                return 'kz_mysql';
+            default:
+                return 'mysql'; // соединение по умолчанию
+        }
+    }
+}
--- config/database.php
+++ config/database.php
@@ -63,6 +63,27 @@ return [
             ]) : [],
         ],
 
+        'kz_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('KZ_DB_DATABASE', 'forge'),
+            'username' => env('KZ_DB_USERNAME', 'forge'),
+            'password' => env('KZ_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
