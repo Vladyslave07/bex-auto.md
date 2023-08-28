2f158d76f16b9f567ec6fe7415132c4b5968326a
Аlex Kudriashov <alexkudriashov1985@gmail.com>
2023-08-28 13:14:08 +0300

    24063 Кнопка кредитования

--- resources/views/livewire/forms/application-for-credit.blade.php
+++ resources/views/livewire/forms/application-for-credit.blade.php
@@ -14,8 +14,61 @@
                             <input wire:model.defer="phone" class="form-control @error('phone') is-invalid @enderror" type="text" placeholder="{{ \App\Models\Domain::phonePlaceholderForCurrDomain() }}" data-type="tel" required>
                             @error('phone')<div class="invalid-feedback">{{ $message }}</div>@enderror
                         </div>
-                        <button class="btn" type="submit">{{ Lang::get('forms.discount.btn') }}</button>
+                        <!-- <button class="btn" type="submit">{{ Lang::get('forms.discount.btn') }}</button> -->
+                        <button class="btn" type="button" onclick="closeModal('#applicationForCredit');openModal('#linksForCredit')">{{ Lang::get('forms.discount.btn') }}</button>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="linksForCredit" class="modal links-credit @if($show === true) is-visible @endif">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="nav-tabs">
+                        <span class="nav-link active" data-toggle="tab" data-target="#creditTab_1">БУ Автомобиль</span>
+                        <span class="nav-link" data-toggle="tab" data-target="#creditTab_2">Новый автомобиль</span>
+                    </div>
+                    <div class="modal-inner">
+                        <svg class="close-modal" width="10" height="10"><use xlink:href="#close-icon"></use></svg>
+                        <div class="main-title text-center">Расчет кредитования</div>
+                        <div class="tab-content">
+                            <div id="creditTab_1" class="tab-pane active">
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    Приватбанк
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    Ощадбанк
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    ПУМБ
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    Пiвденний
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                            </div>
+                            <div id="creditTab_2" class="tab-pane">
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    Ощадбанк
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                                <div class="item">
+                                    <img width="60" height="60" src="https://placehold.jp/E53934/ffffff/60x60.png" loading="lazy" alt="">
+                                    ПУМБ
+                                    <a href="#" class="btn">Рассчитать</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
