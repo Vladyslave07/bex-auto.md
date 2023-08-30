c7635c631bf2ef5369fc6cca5f52944da0d637a8
Аlex Kudriashov <alexkudriashov1985@gmail.com>
2023-08-30 18:15:51 +0300

    24094 Изменение страниц дилерских услуг

--- resources/js/dealer.js
+++ resources/js/dealer.js
@@ -1,11 +1,13 @@
 import Common from './import/common';
 import Phone from './import/phone';
+import Tabs from './import/tabs';
 import CountriesSwiper from './import/countries-swiper';
 import TeamSwiper from './import/team-swiper';
 import Modal from './import/modal';
 
 Common();
 Phone();
+Tabs();
 CountriesSwiper();
 TeamSwiper();
 Modal();
