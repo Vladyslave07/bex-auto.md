38e7f5dd938188e7404a218c98ebbb3ae11a4b5e
Igor_Bashuk <igor@redchameleon.com.ua>
2023-12-07 10:42:17 +0300

    25111 - fix menu

--- resources/views/components/header-menu.blade.php
+++ resources/views/components/header-menu.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="menu-info">
     @if ($items->count() > 0)
-        <nav class="menu-info">
+        <nav>
             @foreach($items as $item)
                 <a href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::localizeUrl($item->link) }}">{{ $item->title }}</a>
             @endforeach
