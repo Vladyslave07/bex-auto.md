d8545a779406968e43ae5dfe86a4567cf6d027d8
Igor_Bashuk <igor@redchameleon.com.ua>
2023-12-15 12:03:29 +0300

    25371 - change editor + remove line

--- resources/views/partials/index/why-we.blade.php
+++ resources/views/partials/index/why-we.blade.php
@@ -1,6 +1,6 @@
 <div class="section-advantages">
     <div class="container">
-        <div class="main-title text-center noline">{!! Setting::get('why_we_block_title') !!}</div>
+        <div class="main-title text-center">{!! Setting::get('why_we_block_title') !!}</div>
     </div>
     @include('partials.index.advantages_list')
 </div>
